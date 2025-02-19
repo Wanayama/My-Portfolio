2f840109e4e86cde8796d40c53c7cb4fc1d364a8
Brian Wanyama <brianwanyama06@gmail.com>
2025-02-19 16:47:10 +0300

    scroll animation

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ function toggleMenu() {
     icon.classList.toggle("open");
   }
   
+
+  // /Scroll reveal
+ScrollReveal({
+  reset: true,
+  distance: '80px',
+  duration: 1000,
+  delay: 50,
+});
+ScrollReveal().reveal('.desktop-nav, .contact', {origin: 'top'});
+ScrollReveal().reveal('.section__pic-container', {origin: 'left'});
+ScrollReveal().reveal('.section__text', {origin: 'right'});
+ScrollReveal().reveal('.about-containers, .text-container, .nav-links', {origin: 'bottom'});
